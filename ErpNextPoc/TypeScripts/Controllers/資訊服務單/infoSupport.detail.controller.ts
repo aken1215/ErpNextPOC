b5e6a61ecd9f5ec2e6220074307c98bdf867dc6f
Hank <aken1215@hotmail.com>
2016-08-01 18:29:48 +0800

    補傳遺漏的檔案

---
+++ ErpNextPoc/TypeScripts/Controllers/資訊服務單/infoSupport.detail.controller.ts
@@ -0,0 +1,32 @@
+﻿module Aurora.ErpNext.Controllers {
+    export class InfoSupportDetailController {
+        static $inject = ['$log', '$state', '$stateParams'];
+        
+        InfoSupports: ViewModel.InfoSupportEditViewModel[];
+
+        constructor(
+            $log: ng.ILogService,
+            protected $state: ng.ui.IStateService,
+            $stateParams: ng.ui.IStateParamsService
+
+        ) {
+            let infoSupport: ViewModel.InfoSupportEditViewModel = {
+                Title: "電腦維修服務",
+                Context: "我的電腦已經壞很久了",
+                ManagerEMPNO: "00012345",
+                ManagerName: "萱姐",
+                Attachments: ["1.jpg", "2.jpg"]
+            }
+            this.InfoSupports = [];
+            this.InfoSupports.push(infoSupport);
+
+        }
+
+        Edit(): void {
+            this.$state.go('infoSupport');
+        }
+    }
+
+    angular.module('Aurora.ErpNext.Controllers')
+        .controller('InfoSupportDetailController', InfoSupportDetailController);
+}
